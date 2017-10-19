17120fe59640a358ecc5ecd8063f7aaa8ef9e178
Victor <victorherrerocazurro@gmail.com>
2017-10-19 13:15:09 +0200

    Coomit final

---
+++ workspace/ProyectoConConfiguracionCompletaBuild/src/main/java/com/prueba/Prueba.java
@@ -0,0 +1,7 @@
+package com.prueba;
+
+public class Prueba {
+	public void metodo(){
+		System.out.println("Hola");
+	}
+}
---
+++ workspace/ProyectoConConfiguracionCompletaBuild/src/test/java/com/curso/ecosistema/PruebaUT.java
@@ -0,0 +1,17 @@
+package com.curso.ecosistema;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.prueba.Prueba;
+
+public class PruebaUT {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		new Prueba().metodo();
+	}
+
+}
